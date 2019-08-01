93ecb693c3489826550e9f639c670ea5b64549a3
“1eyregit config --global user.email lesanfu94@gmail.comgit config --global user.name “1eyre <lesanfu94@gmail.com>
2019-08-01 20:05:44 +0200

    Mi versión

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
+use src\view\FrontController;
+use src\view\Routing;
+
 require_once __DIR__.'/../vendor/autoload.php';
+
+$front = new FrontController();
+var_dump($front);
+//$front -> run();
