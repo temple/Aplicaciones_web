c50a427e2504905dbc4db9184b5bf542dc36f6c9
Jordi <gonzalezsanchezjordi@gmail.com>
2019-08-05 17:03:20 +0200

    Modificacion de Index.php y FrontController.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,11 @@
 <?php
 
-include __DIR__.'/vista.html';
-exit;
 
 require_once __DIR__.'/../vendor/autoload.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-//require __DIR__.'/../src/FrontController.php';
 
 $front = new FrontController();
-$front->run();
 
+$front->run();
+var_dump($front);
