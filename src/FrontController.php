d6e40d04f598d4254d3401219ff3a27c108ef754
Xavier Subirà <el.temple@gmail.com>
2019-08-05 15:52:25 +0200

    Challenge: Front controller Haz que el controlador frontal utilice información del servidor. Esta información permitirá determinar la ruta que ha solicitado el usuario. El usuario envía una Request HTTP para solicitar rutas (conocidas como URI).

--- src/FrontController.php
+++ src/FrontController.php
@@ -9,4 +9,11 @@ class FrontController
 		$this->routing = new Routing();
 	}
 
+	//TODO: Que obtenga información del servidor en la que haya la ruta solicitada por el usuario en la request
+	// p.e. que lo guarde en una variable una vez lo haya conseguido.
+	
+	//TODO: Esto lo tiene que hacer FrontController
+	// en una función.
+	// crea esta función. No olvides mirar el código de public/index.php 
+
 }
