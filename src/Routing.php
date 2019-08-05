0f278dd2c06d0bd405cf535007323a2899b444ca
Xavier Subirà <el.temple@gmail.com>
2019-08-05 18:49:12 +0200

    Challenge: RoutingInterface Logra que la clase Routing cumpla con la interfaz RoutingInterface.

--- src/Routing.php
+++ src/Routing.php
@@ -3,7 +3,9 @@
 
 use controller\HomeController;
 
-class Routing{
+class Routing
+	implements RoutingInterface
+{
 
 	/**
 	 * Objeto de tipo Routes
@@ -15,9 +17,10 @@ class Routing{
 		$this->buildRoutes();
 	}	
 
-
 	// TODO: Implementar el método buildRoutes
 	// la propiedad $routes tendrá que responder TRUE
-		// a la pregunta $this->routes instanceof \StdClass
+	// a la pregunta $this->routes instanceof \StdClass
 
+	// TODO: Implementar las funciones necesarias 
+	// para que Routing cumpla con la interfaz RoutingInterface 
 }
