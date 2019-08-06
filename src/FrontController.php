375968951cb5c963744a12a179de8d505a9b8c7b
Carmen Custodio <chantra132@gmail.com>
2019-08-06 15:43:45 +0200

    FRONTCONTROLLER Eliminado ruido, añadido DONE y eliminado $routes no necesario

--- src/FrontController.php
+++ src/FrontController.php
@@ -1,7 +1,5 @@
 <?php
 
-//ini_set('display_errors', true);
-//require_once __DIR__.'/../vendor/autoload.php';
 
 class FrontController
 {
@@ -13,18 +11,18 @@ class FrontController
 
 	}
 
-	//TODO: Que obtenga información del servidor en la que haya la ruta solicitada por el usuario en la request
+	//DONE Que obtenga información del servidor en la que haya la ruta solicitada por el usuario en la request
 	// p.e. que lo guarde en una variable una vez lo haya conseguido.
 	
-	//TODO: Esto lo tiene que hacer FrontController
+	//DONE Esto lo tiene que hacer FrontController
 	// en una función.
 	// crea esta función. No olvides mirar el código de public/index.php 
  	public function run(){
  		$ruta = $_SERVER['REQUEST_URI'];
- 		//var_dump("la ruta añadida a la dirección URL es:". $ruta);
  		
- 		$this->request_uri	= $ruta;
- 		var_dump($this->request_uri.  "\r");
- 	}
+ 		
+ 		$this->request_uri;
+ 	
+ 		 	}
 
 }
