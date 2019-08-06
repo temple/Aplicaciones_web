c73944722c4f7e1f7c2c58c74a058d7b49664759
Carmen Custodio <chantra132@gmail.com>
2019-08-06 20:14:16 +0200

    Challenge RoutingComponent completo.

--- src/Routing.php
+++ src/Routing.php
@@ -3,6 +3,7 @@
 
 use controller\HomeController;
 
+
 class Routing{
 
 	/**
@@ -15,11 +16,11 @@ class Routing{
 		$this->buildRoutes();
 	}	
 	public function buildRoutes(){
-		$this->routes = new StdClass();
-		var_dump($this->routes instanceof \StdClass);
+		$routes = $GLOBALS['config'] ['routes'];
+		var_dump($routes instanceof \StdClass);
 	}
 	
-	// TODO: Implementar el método buildRoutes
+	// DONE: Implementar el método buildRoutes
 	// la propiedad $routes tendrá que responder TRUE
 	// a la pregunta $this->routes instanceof \StdClass
 
