981bfa2ac520c39121571e3df7c959d5e44d555f
Carmen Custodio <chantra132@gmail.com>
2019-08-07 19:33:28 +0200

    Se crea función getAction

--- src/Routing.php
+++ src/Routing.php
@@ -4,7 +4,7 @@
 use controller\HomeController;
 
 class Routing
-	implements RoutingInterface
+	//implements RoutingInterface
 {
 
 	/**
@@ -22,15 +22,29 @@ class Routing
 		$this->routes = json_decode($content); 
 		// Guardamos en content el contenido (string) del archivo de rutas
 		$this->routes = $this->routes instanceof \StdClass
-						? $this->routes 
-						: $GLOBALS['config'] ['routes']; 
+		? $this->routes 
+		: $GLOBALS['config'] ['routes'];
+		
 	}
 	
 	// DONE: Implementar el método buildRoutes
 	// la propiedad $routes tendrá que responder TRUE
 	// a la pregunta $this->routes instanceof \StdClass
-	public function getController(string $uri) : IdxController;
-	public function getAction(string $uri) : string;
+	public function getController(string $uri) : IdxController{
+
+	}
+
+
+	public function getAction(string $uri) : string{
+		$routesArray =  (array) $this->routes; // $this->routes es un objeto, se modifica a array.
+		$uri =ltrim($uri , "/");// se crea variable donde se elimina "/" de $uri
+		$result = $routesArray [$uri]->action; // se crea variable donde guardamos string que queremos 
+		return is_null($result) // si $result es null o no está creado 
+				?"index" //retorna "index"
+				:$result; // si no es null retorna $result
+
+	}
+	
 	// TODO: Implementar las funciones necesarias 
 	// para que Routing cumpla con la interfaz RoutingInterface 
 }
