159bf7c14f6718dba5bc03fe75c7a1a1f4d4714c
Leyre Sánchez <lesanfu94@gmail.com>
2019-08-07 15:51:52 +0200

    Ruido eliminado

--- public/index.php
+++ public/index.php
@@ -1,14 +1,8 @@
 <?php
 
-//include __DIR__.'/vista.html';
-
-
 include __DIR__.'/../src/config.php';
 require_once __DIR__.'/../vendor/autoload.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-//require __DIR__.'/../src/FrontController.php';
 
 $front = new FrontController();
-//$front->run();
-//exit;
