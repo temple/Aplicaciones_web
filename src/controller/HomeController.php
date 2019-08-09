c93187422a757285db282e0febea3341bb08e911
Leyre Sánchez <lesanfu94@gmail.com>
2019-08-09 17:57:52 +0200

    Challenge: HTML Response- Sense acabar

--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -6,5 +6,11 @@ class HomeController
 	extends AbstractController
 {
 
+	function index ($this->request_uri,$this->params){
+ 		$vista = file_get_contents(__DIR__."../view/home.html");
+		echo $vista;
+ 	}
+
+
 	//TODO: Crea el método adecuado para que se muestren las vistas correspondientes a las acciones: index y premium
 }
