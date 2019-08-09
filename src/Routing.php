e7091e4a534d8d3e961bf4620c50c4761cba78ef
kyomi <kyomiguidet@gmail.com>
2019-08-09 19:03:02 +0200

    kyomi-routing

--- src/Routing.php
+++ src/Routing.php
@@ -7,7 +7,7 @@ class Routing{
 
 	/**
 	 * Objeto de tipo Routes
-	 * @var \StdClass
+	 * var \StdClass
 	 */
 	protected $routes;
 
@@ -15,9 +15,15 @@ class Routing{
 		$this->buildRoutes();
 	}	
 
+	public function buildRoutes(){
+		$content = file_get_contents(__DIR__."/config.php");
+		$this->routes = new \StdClass;
+		var_dump($this->routes instanceof \StdClass);
+	}
 
 	// TODO: Implementar el método buildRoutes
 	// la propiedad $routes tendrá que responder TRUE
 		// a la pregunta $this->routes instanceof \StdClass
+	//Logra que en la propiedad $routes de Routing se carguen las rutas de config.php o alternativamente de routes.json
 
 }
