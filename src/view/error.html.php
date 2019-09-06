7f975e614c42e9e631b0dd3dc566a98f35716748
Carmen Custodio <chantra132@gmail.com>
2019-09-06 16:29:40 +0200

    Se crean las vistas para poder realizar verificación de funcionamiento.

---
+++ src/view/error.html.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ERROR - Aplicaciones-Web</title>
+</head>
+
+<body>
+	
+	ESTA EL A PAGINA DE ERROR
+</body>
+</html>
---
+++ src/view/home.html.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>HOME - Aplicaciones_Web</title>
+</head>
+
+<body>
+	
+	ESTA ES LA PAGINA DE INICIO / HOME PAGE
+</body>
+</html>
