ab9ca7871fa1404bea21c1ddf3a90775d6b200e5
itainn <itain.sys@gmail.com>
2019-09-06 15:42:03 +0200

    variable que guarda informacion de routes.json codificado archivo en json a una  variable de PHP

--- src/Routing.php
+++ src/Routing.php
@@ -13,10 +13,22 @@ class Routing{
 
 	public function __construct(){
 		$this->buildRoutes();
+	}
+	// TODO: Implementar el método buildRoutes
+	public function buildRoutes(){
+		$contents = file_get_contents(__DIR__."/config/routes.json");
+		//guardo en la variable $contents la informacion de routes.json
+		// file_get_contents me devuelve un string que contiene todos los elementos del array del archivo routes.json
+		$this->routes = json_decode($contents);
+		// Convierte un string codificado en JSON a una variable de PHP.
+
+		$this->routes = $this->routes instanceof \StdClass
+		
+
 	}	
 
 
-	// TODO: Implementar el método buildRoutes
+	
 	// la propiedad $routes tendrá que responder TRUE
 		// a la pregunta $this->routes instanceof \StdClass
 
