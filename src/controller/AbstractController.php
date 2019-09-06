07548682cc5b46785b0c85434cfeabb1c934f8bf
Carmen Custodio <chantra132@gmail.com>
2019-09-06 21:59:10 +0200

    Se vuelve a integrar el código que había en challenge anterior.

--- src/controller/AbstractController.php
+++ src/controller/AbstractController.php
@@ -11,8 +11,10 @@ abstract class AbstractController{
 	 * Controller what shows an error page
 	 * @var IndexControllerInterface
 	 */
-
+	//protected $indexAction;
+	//protected $premiumAction;
 	protected $errorController;	
+
 
 	// TODO:
 	// es: hacer que en la construcción de una nueva instancia se inicialice la propiedad errorController
@@ -35,41 +37,49 @@ abstract class AbstractController{
 	{
 		$method = $action;
 		//  Si la acción solicitada no termina en "Action"..
-		if (FALSE == preg_match('/\w\w*Action$/', $action))
-			$method = $action."Action";
+		if (FALSE == preg_match('/\w\w*Action$/', $action)){
+				$method = $action."Action";
+				$array_params_request = [$request , $params];			
 		// le añadimos "Action" al final
+		}
 		try{
-			if ( method_exists($this, $method) )			
-					return call_user_func_array([$this,$method], $params);
-				// TODO:
-				// es: Piensa e implementa una solución para que el parámetro $request no se pierda
-				// en: Plan a solution in order to use the $request parameter, which is missed currently
-			else
-				$error = PageNotFoundError::getInstance();
-				$error->setDetails(new \Exception("Action $action was not found"));
-				throw $error;
 				
+			if ( method_exists($this, $method) ){			
+			$array_params_request = [$request , $params];
+			//var_dump($method, $params , $request);
+			return call_user_func_array( [$this,$method], $array_params_request);
+
+
+				// DONE:
+				// es: Piensa e implementa una solución para que el parámetro $request no se pierda
+			}	// en: Plan a solution in order to use the $request parameter, which is missed currently
+			else {
+						$error = PageNotFoundError::getInstance();
+						$error->setDetails(new \Exception("Action $action was not found"));
+					}
+			throw $error;			
 		}
 		catch (\Throwable $error){
 			return $this->showError($request, $error, $action, $params);
-		}
+			}
 	}
 
-	/**
-	 * Muestra un error
+	 /* Muestra un error
 	 */
-	protected function showError($request, \Throwable $error =null, string $action ='index', array $params=[])
-	{
-		if ($this->errorController instanceof IndexControllerInterface){
-			$params = array('action'=>$action,'error'=>$error, 'params'=>$params);
-			$this->errorController->indexAction($request,$params);
-			//$this->errorController->callAction($request,'index',$params);
-		}
-		else{
-			if (!($error instanceof \Throwable))
-				$error = new \Exception('Uknown Exception',500);
-			
-			throw $error;
-		}
+	 protected function showError($request, \Throwable $error =null, string $action ='index', array $params=[])
+	 {
+
+	 	if ($this->errorController instanceof IndexControllerInterface){
+	 	 		$params = array('action'=>$action,'error'=>$error, 'params'=>$params);
+	 	 		$this->errorController->indexAction($request,$params);
+
+	 			//$this->errorController->callAction($request,'index',$params);
+	 	}
+	 	else{
+	 	 		if (!($error instanceof \Throwable)){
+	 			$error = new \Exception('Uknown Exception',500);
+	 		}
+	 		throw $error;
+	 	}
+	 }
 	}
-}
