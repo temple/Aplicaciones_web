dd6fdfcbe870856eb8ab8cf9f131f3c9e0d3eb46
sandyr73 <sandy>
2019-09-09 19:01:51 +0200

    CHALLENGE HTML-RESPONSE

---
+++ src/view/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>HOme</title>
+</head>
+<body>
+	
+<?php
+	echo "pagina de index - home";
+	var_dump($request, $params);
+?>
+
+</body>
+</html>
