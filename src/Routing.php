81a3a3492b991ff0c36c9822c24d6a9156c63e0e
cevelta <alejandrea.velasquez16c@gmail.com>
2019-09-09 16:14:04 +0100

    challenge Routing

--- src/Routing.php
+++ src/Routing.php
@@ -19,5 +19,18 @@ class Routing{
 	// TODO: Implementar el método buildRoutes
 	// la propiedad $routes tendrá que responder TRUE
 		// a la pregunta $this->routes instanceof \StdClass
+	
+public function buildRoutes(){
+		
+		$content = file_get_contents(_DIR_."/config/routes.json");
+		//contenido JSON
+		$this->routes = json_decode($content); 
+
+		$this->routes = $this->routes instanceof \StdClass
+						? $this->routes 
+						: $GLOBALS['config'] ['routes']; 
+
+	}
+
 
 }
