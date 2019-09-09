3a0c748e47bfdb933f805b399c6b796993c01a40
sandyr73 <sandy>
2019-09-09 20:01:00 +0200

    vista premium

---
+++ src/view/homepremium.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>HOme Premium</title>
+</head>
+<body>
+		<h1> Página Premium</h1>
+<?php
+	echo "pagina de home premium";
+	var_dump($request, $params);
+?>
+
+</body>
+</html>
