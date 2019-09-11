1179f461c520202e958cea62bfa755e725dea39a
Carmen Custodio <chantra132@gmail.com>
2019-09-12 01:35:36 +0200

    Se realizan TODO: Implementa la acción que recogerá los datos enviados TODO: Haz que se verifique la $request recibida y que estén todos los datos que se esperan, dentro del parámetro $params TODO: Completa la implementación de indexAction

--- src/controller/ContactController.php
+++ src/controller/ContactController.php
@@ -1,21 +1,42 @@
 <?php
 
-class ContactController{
+namespace 	controller;
 
-	//TODO: Implementa la acción que recogerá los datos enviados 
+class ContactController
+extends AbstractController
+{
+	public $request;
+	public $params;
+	//DONE: Implementa la acción que recogerá los datos enviados 
 	//      mediante el envio por AJAX de un formulario
 	//      la acción se llamará processContactAjaxAction
-	public function ...... {
-		// TODO: Haz que se verifique la $request recibida
+	public function processContactAjaxAction ($request ,array $params) {
+		// DONE: Haz que se verifique la $request recibida
 		//       y que estén todos los datos que se esperan,
 		//       dentro del parámetro $params
+	
+		if ($_POST['email']==""){
 
-		return json_encode($message);
+			$message =  "No incluyo ningún email. Por favor, revise el formulario";
+			//echo json_encode ($message); 
+		} 
+		elseif ($_POST['asunto']==""){
+			$message = "No incluyo ningún asunto. Por favor, revise el formulario";
+		}
+		elseif ($_POST['mensaje']=="") {
+			$message = "No incluyo ningún mensaje. No tiene nada que decirnos???? 
+			Por favor, revise el formulario";
+		}
+		else { //si todo está relleno ;
+			$message =  "Tu mensaje se ha enviado correctamente. En breve recibirá contestación."; 
+			//echo json_encode($message);
+		}
+		echo json_encode($message);
 	}
 
-	//TODO: Completa la implementación de indexAction
+	//DONE: Completa la implementación de indexAction
 	//      para que muestre un formulario que funcione via ajax
-	public function indexAction(){
-		include "../view/contact.html.php";
+public function indexAction($request , $params){
+	include __DIR__."/../view/contact.html.php";
 	}
 }
