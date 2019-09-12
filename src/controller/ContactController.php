a677bd7671e0b67d711740e6db16fc75d3fd431f
sandyr73 <sandy>
2019-09-12 21:12:22 +0200

    error linea 1 solucionado

--- src/controller/ContactController.php
+++ src/controller/ContactController.php
@@ -2,30 +2,30 @@
 namespace controller;
 class ContactController
 	extends AbstractController
-{
-	//TODO: Implementa la acción que recogerá los datos enviados
-	//      mediante el envio por AJAX de un formulario
-	//      la acción se llamará processContactAjaxAction
-	public function processContactAjaxAction($request ,array $params){
- 
-		sleep(2);
- 		
-		if((!(empty($params['email']))) && (!(empty($params['nombre']))) && (!(empty($params['msj'])))){
+	{
+		//TODO: Implementa la acción que recogerá los datos enviados
+		//      mediante el envio por AJAX de un formulario
+		//      la acción se llamará processContactAjaxAction
+		public function processContactAjaxAction($request ,array $params){
+	 
+			sleep(2);
+	 		
+			if((!(empty($params['email']))) && (!(empty($params['nombre']))) && (!(empty($params['msj'])))){
 
-			$massage = "DATOS RECIBIDOS CORRECTAMENTE";
-		
-		}else{
+				$massage = "DATOS RECIBIDOS CORRECTAMENTE";
+			
+			}else{
 
-			$massage = "FALSE";
+				$massage = "FALSE";
+			}
+
+			echo json_encode($massage);
 		}
 
-		echo json_encode($massage);
-	}
 
-
-	//TODO: Completa la implementación de indexAction
-	//      para que muestre un formulario que funcione via ajax
-	public function indexAction($request , $params){
-		include  __DIR__."/../view/contact.html.php";
+		//TODO: Completa la implementación de indexAction
+		//      para que muestre un formulario que funcione via ajax
+		public function indexAction($request , $params){
+			include  __DIR__."/../view/contact.html.php";
+		}
 	}
-}
