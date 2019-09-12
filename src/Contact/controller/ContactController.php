8be31194af12f822ad1814d9fd5b93889124fa4d
Leyre Sánchez <lesanfu94@gmail.com>
2019-09-12 20:58:53 +0200

    Challenge Ajax

--- src/Contact/controller/ContactController.php
+++ src/Contact/controller/ContactController.php
@@ -3,23 +3,46 @@
 namespace Contact\controller;
 use controller\AbstractController as IdxController;
 
-class ContactController{
+class ContactController
 		 extends IdxController
 
+{
 	//TODO: Implementa la acción que recogerá los datos enviados 
 	//      mediante el envio por AJAX de un formulario
 	//      la acción se llamará processContactAjaxAction
-	public function ...... {
+	public function processContactAjax($request,$params) {
+		
+		
+		sleep(2);
+		//       dentro del parámetro $params	 		
+		if((!(empty($params['email']))) && (!(empty($params['nombre']))) && (!(empty($params['mensaje'])))){
+			$massage = "DATOS RECIBIDOS CORRECTAMENTE";
+		
+		}else{
+		return json_encode($message);				
+		$massage = "FALSE";
+		}
+		echo json_encode($massage);
+			}
+
+		/*var_dump($email);
+			if(!empty($email)){ //si la variable NO está vacía
+				echo json_encode("Email recibido OK");
+				}else{
+				echo json_encode("Email recibido OK");
+			}
+			*/
+
+	
 		// TODO: Haz que se verifique la $request recibida
 		//       y que estén todos los datos que se esperan,
 		//       dentro del parámetro $params
 
-		return json_encode($message);
-	}
+
 
 	//TODO: Completa la implementación de indexAction
 	//      para que muestre un formulario que funcione via ajax
 	public function indexAction(){
-		include "../view/contact.html.php";
+		include  __DIR__."/../view/vista-contact.php";
 	}
 }
---
+++ src/Contact/view/vista-contact.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+   <header>
+  <title>Materi.es</title>
+  <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/index.css">
+    <!-- Framework Jquery-->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/index.js"></script>
+    <script src="https://use.fontawesome.com/eaf3553229.js"></script>
+	
+      <img class="logo" alt="logo corporativo" src="http://drive.google.com/file/d/1iX9YP8gmbv9BDQTJLK1DNaHv-B5Nvg89/view" />
+        <nav>
+       <ul class="rojo negrita">
+              <li>Matematicas</li>
+              <li>Ciencias naturales</li>
+              <li>Inglés</li>
+              <li>Materi.es</li>
+          </ul>
+      </nav>
+  
+  <nav class="buscador">
+   <div class="busqueda">
+      <input type="text" class="searchTerm" placeholder="Quiero buscar...">
+      <button type="submit" class="searchButton">
+        <img class="busqueda" src="https://image.flaticon.com/icons/png/512/1096/1096351.png" alt="lupa">
+
+        <i class="fa fa-search"></i>
+     </button>
+   </div>
+</nav>
+
+
+  <nav class="acceso">
+       <ul>
+         <li><button>Entrar al aula</button></li>
+         <li><button>Registrarse</button></li>
+
+          </ul>
+      </nav>
+  
+      <form id="Idioma">
+          <input id="cat-lang" value="ca-ES" type="radio" name="idioma" />
+          <label for="cat-lang"><img src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Flag_of_Catalonia.svg" width="30" alt="Català" /></label>
+          <input id="es-lang" value="es-ES" type="radio" name="idioma" />
+          <label for="es-lang"><img src="https://www.comprarbanderas.es/images/banderas/400/1-espana_400px.jpg" width="30" alt="castellano" /></label>
+          <input id="eng-lang" value="en-EN" type="radio" name="idioma" />
+          <label for="eng-lang"><img src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_States.svg" width="30" alt="english" /></label>
+      </form>
+    </header>
+  </head>
+<body>
+  <h2>Contacta con nosotros</h2>
+
+  	
+		<form class = "form" method="post" id="ajaxform">
+			<h3>Ajax form</h3>
+			<input class= "element" type="text" id="nombre" name="nombre" placeholder="Nombre">
+			<input  class= "element" type="email" id="email" name="email" placeholder="Email">
+			<input class= "element" type="text" id="mensaje" name="mensaje" placeholder="Mensaje">
+			<input class= "element" type="button" id="btn" value="Enviar Form">
+			<!--Caja de mensajes de validación y respuesta del server -->
+			<div id="data"></div>
+		</form>
+	   
+    <footer>
+    <div class="row">
+      <ul title="zona asignaturas">
+      <li><a href="#" title="">Matemáticas</a></li>
+      <li><a href="#" title="">Ciencias naturales</a></li>
+      <li><a href="#" title="">Inglés</a></li>
+    </ul>
+  <ul title="zona contacto">
+        <li><b>Siguenos en:</b>
+      </li>
+         <img class="imagen"alt="Instagram" src="***" />
+         <img class="imagen"alt="Youtube" src="***" />
+         <img class="imagen"alt="Facebook" src="***" />
+
+    </ul>   
+     <ul title="zona legal">
+      <li><a href="#" title="">Aviso legal</a></li>
+      <li><a class="hola" href="#" title="">Política de privacidad</a></li>
+      <li><a href="#" title="">Política de cookies</a></li>
+    </ul>
+    </div>
+    <p class="copyr">&copy; Materies 2019</p>
+  </footer>
+
+</body>
+</html>
