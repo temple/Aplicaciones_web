c63db9cff49b9c3984244d29fc1fb88dadac4579
walaa <w.anwar.khalifa@gmail.com>
2019-09-12 21:04:08 +0200

    routingInterface done

--- src/Routing.php
+++ src/Routing.php
@@ -2,6 +2,7 @@
 
 
 use controller\HomeController;
+use controller\AbstractController as IdxController;
 
 class Routing
 	implements RoutingInterface
@@ -24,11 +25,26 @@ class Routing
 		$this->routes = $this->routes instanceof \StdClass
 						  
 	}
-
-	// TODO: Implementar el método buildRoutes
-	// la propiedad $routes tendrá que responder TRUE
-	// a la pregunta $this->routes instanceof \StdClass
-
+	public function getController(string $uri) : IdxController{
+		$routesArray =  (array) $this->routes; 
+		$uri =ltrim($uri , "/");
+		$result = $routesArray [$uri]->controller;queremos 
+		$result = is_null($result) 
+				?"controller\\ErrorController" 
+				:"controller\\".$result;
+		$reflector = new \ReflectionClass($result); 
+		return $reflector->newInstance(); 
+	}
+	public function getAction(string $uri) : string{
+		$routesArray =  (array) $this->routes; 
+		$uri =ltrim($uri , "/");
+		$result = $routesArray [$uri]->action; 
+		return is_null($result) 
+				?"index" 
+				:$result; 
+	}
+	
 	// TODO: Implementar las funciones necesarias 
 	// para que Routing cumpla con la interfaz RoutingInterface 
 }
+
