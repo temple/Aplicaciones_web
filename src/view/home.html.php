b89166ab3fc5debef4cd831ff754b9057fff4d30
walaa <w.anwar.khalifa@gmail.com>
2019-09-13 20:40:23 +0200

    html file

---
+++ src/view/home.html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML RESPONSE</title>
+</head>
+<body>
+	<?php
+	 echo "this is HTML response";
+
+	 var_dump($request, $params); 
+
+	?>
+
+</body>
+</html>
