e453a3a368111e0d1007357d62133543b3c0a104
itainn <itain.sys@gmail.com>
2019-09-17 00:43:36 +0200

    eleminacion de php inservibe

--- src/controller/ContactController.php
+++ src/controller/ContactController.php
@@ -12,19 +12,7 @@ class ContactController
 	public function processContactAjaxAction ($request ,array $params) 
 	{//creacion del metodo processContactAjax
 
-		if ($_POST['email']==""){
-			$message =  "No incluyo ningún email. Por favor, revise el formulario";
-			//echo json_encode ($message); 
-		} 
-		elseif ($_POST['asunto']==""){
-			$message = "No incluyo ningún asunto. Por favor, revise el formulario";
-		}
-		elseif ($_POST['mensaje']=="") {
-			$message = "No incluyo ningún mensaje. No tiene nada que decirnos???? 
-			Por favor, revise el formulario";
-		}
-		else { //si todo está relleno ;
-			$message =  "Tu mensaje se ha enviado correctamente. En breve recibirá contestación."; 
+		
 			
 		}
 		echo json_encode($message);
